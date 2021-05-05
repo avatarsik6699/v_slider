4f9ad2f47bf8d7abf9c2c18d1482cc9a2e07af2f
Vlad Godlevskii <avatarsik6699@gmail.com>
2021-05-06 02:51:29 +0700

    feat: added webpack config

---
+++ dist/index.05c95b7ae3866f2e2837.js
@@ -0,0 +1,1 @@
+(self.webpackChunkslider=self.webpackChunkslider||[]).push([[826],{108:function(){"use strict";var e=document.createElement("div");e.textContent="kek",document.append(e)}},function(e){"use strict";var t;t=108,e(e.s=t)}]);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-console.log('hello');
-const a = 1;
-const b = 2;
+// console.log('hello');
+// const a = 1;
+// const b = 2;
 
-if (a > b || true === true || true) {
-}
+// if (a > b || true === true || true) {
+// }
+// console.log(1);
+const el = document.createElement('div');
+el.textContent = 'kek';
+document.append(el);
+// kek
